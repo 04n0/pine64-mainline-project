7c9b4111a3331bb9fcedae1b2608d60efed2c330
04n0 <04n0@604.sk>
2018-02-08 16:43:51 +0100

    Pull base Docker image before the build starts

--- build-docker-engine.sh
+++ build-docker-engine.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -eu
+
 VERSION="v17.12.0-ce"
+BASEIMAGE="$(head -n1 config/docker-Dockerfile.aarch64|sed 's|FROM ||g')"
 
 echo "This has to be run on the PINE itself! Cross compiling is crazy annoying apparently..."
 
 pushd docker
 git clone https://github.com/docker/docker-ce.git --depth 1 --single-branch -b ${VERSION}
 cp config/docker-Dockerfile.aarch64 docker/docker-ce/components/packaging/deb/debian-stretch/Dockerfile.aarch64
+docker pull ${BASEIMAGE}
 pushd docker-ce/components/packaging/deb
 make debian-stretch
 popd
